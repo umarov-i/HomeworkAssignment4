91abd36376b67f4fd8c9a5727ef73e50b8da005d
umarov-i <i.umarov@newuu.uz>
2024-10-28 17:57:05 +0500

    week4

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
+#include "problems.h"
 using namespace std;
-void decimalToBinary(int n) {
-    if (n == 0) {
-        return;
-    }
-
-    decimalToBinary(n / 2);
-
-    cout << (n % 2);
-}
-
 int main() {
-    decimalToBinary(5);
+    exampleFunction();
     return 0;
 }
